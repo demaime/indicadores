dafc6f431c9743398b243530dd89430a63a2b33f
Demian <aimedemian@gmail.com>
2024-03-05 12:14:49 -0300

    primer commit con test de grafs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Inflacion from "./components/Inflacion.jsx";
+import Transporte from "./components/Transporte.jsx";
 
-function App() {
+export default function App() {
+  const [dataInflacion, setDataInflacion] = useState("nacional");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-full">
+      <h1 className="p-2 text-center w-full text-3xl font-bold underline">
+        Indicadores de Realidad
+      </h1>
+      <section className="w-full h-52 flex">
+        <div className="w-1/6 h-full bg-green-500 flex flex-col items-center justify-around">
+          <button
+            className="p-2 rounded-xl bg-green-200 w-32"
+            onClick={() => setDataInflacion("nacional")}
+          >
+            Nacional
+          </button>
+          <button
+            className="p-2 rounded-xl bg-green-200 w-32"
+            onClick={() => setDataInflacion("caba")}
+          >
+            CABA
+          </button>
+        </div>
+        <div className="w-5/6 h-full bg-green-200">
+          <Inflacion data={dataInflacion} />
+        </div>
+      </section>
+      <h2 className="text-center font-bold">Boletos mínimos</h2>
+      <section className="w-full h-96 bg-gray-900 flex items-center">
+        <Transporte />
+      </section>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Inflacion.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { BarChart, XAxis, YAxis, Tooltip, Bar } from "recharts";
+
+const nacional = [
+  { mes: "Diciembre", valor: 32 },
+  { mes: "Enero", valor: 20.6 },
+  { mes: "Febrero", valor: 18 },
+  { mes: "Marzo", valor: 21.1 },
+  { mes: "Abril", valor: 15 },
+  { mes: "Mayo", valor: 13 },
+];
+
+const caba = [
+  { mes: "Diciembre", valor: 28 },
+  { mes: "Enero", valor: 21.7 },
+  { mes: "Febrero", valor: 19 },
+  { mes: "Marzo", valor: 35 },
+  { mes: "Abril", valor: 30 },
+  { mes: "Mayo", valor: 16.7 },
+];
+
+export default function Inflacion({ data }) {
+  console.log(data);
+  const CustomLabel = ({ x, y, width, height, value }) => {
+    return (
+      <text
+        x={x + width / 2}
+        y={y + height / 2}
+        fill="#FFFFFF"
+        textAnchor="middle"
+        dominantBaseline="middle"
+      >
+        {value}
+      </text>
+    );
+  };
+
+  return (
+    <>
+      <h2 className="text-center p-2 text-xl font -bold text-[#8884d8]">
+        Inflación
+      </h2>
+      <div className="w-full flex justify-center items-center">
+        <BarChart
+          width={1200}
+          height={150}
+          data={data === "nacional" ? nacional : caba}
+        >
+          <XAxis dataKey="mes" />
+          <YAxis />
+          <Tooltip />
+          <Bar dataKey="valor" fill="#8884d8" label={<CustomLabel />} />
+        </BarChart>
+      </div>
+    </>
+  );
+}

32d513cdac0db41b918a5c255371c41537b6113e
Demian <aimedemian@gmail.com>
2024-04-17 09:55:10 -0300

    rdy

--- src/components/ServiciosMes.jsx
+++ src/components/ServiciosMes.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
-export default function ServiciosMes({ mes }) {
-  return <div className="w-full h-full bg-yellow-200">{mes}</div>;
-}
+const ServiciosMes = ({ mes }) => {
+  return (
+    <div className="w-full h-full bg-yellow-200 flex justify-center items-center relative">
+      <div className="w-20 h-20 rounded-full bg-red-400 absolute top-2 left-2"></div>
+      <div className="w-20 h-20 rounded-full bg-red-400 absolute bottom-2 left-2"></div>
+      <div className="w-20 h-20 rounded-full bg-red-400 absolute top-2 right-2"></div>
+      <div className="w-20 h-20 rounded-full bg-red-400 absolute bottom-2 right-2"></div>
+      <div className="w-20 h-20 rounded-full bg-red-400 absolute top-1/4 left-2"></div>
+      <div className="w-20 h-20 rounded-full bg-red-400 absolute top-1/4 right-2"></div>
+      <div className="w-20 h-20 rounded-full bg-red-400 absolute bottom-1/4 left-2"></div>
+      <div className="w-20 h-20 rounded-full bg-red-400 absolute top-1/4 right-2"></div>
+    </div>
+  );
+};
+
+export default ServiciosMes;

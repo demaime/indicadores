f7ad5b6ef46d64bcc9421ff8ca365c723c1eef05
Demian <aimedemian@gmail.com>
2024-10-24 15:27:23 -0300

    iniciado informemensual

--- src/components/InformeMensual/Slide1.jsx
+++ src/components/InformeMensual/Slide1.jsx
@@ -6,7 +6,7 @@ export default function Slide1() {
     <div className="w-full h-full flex flex-col items-center justify-around px-8">
       <div className="w-1/2 flex flex-col items-center justify-between h-16 tracking-wider">
         <h1 className=" !text-5xl font-bold ">Conclusiones</h1>
-        <div className="w-3/4 h-2 rounded-xl bg-red-400 relative">
+        <div className="w-3/4 h-2 rounded-xl bg-blue-400 relative">
           <div className="rounded-full h-2 w-2 bg-gray-900 absolute right-4"></div>
           <div className="rounded-full h-4 w-4 bg-gray-900 absolute right-8 -top-1"></div>
           <div className="absolute left-8 -top-3 text-gray-900 font-black text-xl">
@@ -16,35 +16,36 @@ export default function Slide1() {
       </div>
 
       <p className="text-center text-3xl flex flex-col text-gray-600">
-        EL CONSUMO DE CARNE VACUNA SE ENCUENTRA EN EL&nbsp;
+        EL SECTOR METALÚRGICO REGISTRÓ SU&nbsp;
         <strong className="text-6xl m-2 text-gray-800">
-          NIVEL HISTÓRICO MÁS BAJO
+          PEOR CAÍDA EN 5 AÑOS
         </strong>
       </p>
       <div className="w-full flex items-center justify-evenly">
-        <div className="w-1/4 h-32 bg-red-700 text-gray-200 text-xl rounded-xl shadow-lg shadow-black text-center flex flex-col items-center justify-end">
-          <p className="w-full h-1/2 text-6xl font-semibold">
-            46.9 <span className="text-xl">kg/hab</span>
+        <div className="w-1/4 h-32 bg-slate-700 text-gray-200 text-xl rounded-xl shadow-lg shadow-black text-center flex flex-col items-center justify-end">
+          <p className="w-full h-1/2 text-6xl font-semibold">-7.3 %</p>
+          <p className="w-full h-1/4 text-xs mt-2 tracking-wider">
+            <strong className="text-blue-400">PRODUCCIÓN</strong> (INTERANUAL)
           </p>
-          <p className="w-full h-1/4 text-xs mt-2">ENE - AGO '24</p>
         </div>
-        <div className="w-1/4 h-32 bg-red-700 text-gray-200 text-xl rounded-xl shadow-lg shadow-black text-center flex flex-col items-center justify-end">
-          <p className="w-full h-1/2 text-6xl font-semibold">-12.1 %</p>
-          <p className="w-full h-1/4 text-xs mt-2">
-            VARIACIÓN INTERANUAL (ENE-AGO '23 / ENE-AGO '24)
+        <div className="w-1/4 h-32 bg-slate-700 text-gray-200 text-xl rounded-xl shadow-lg shadow-black text-center flex flex-col items-center justify-end">
+          <p className="w-full h-1/2 text-6xl font-semibold">50.1 %</p>
+          <p className="w-full h-1/4 text-xs mt-2 tracking-wider">
+            UTILIZACIÓN DE{" "}
+            <strong className="text-blue-400"> CAPACIDAD INSTALADA</strong>
           </p>
         </div>{" "}
-        <div className="w-1/4 h-32 bg-red-700 text-gray-200 text-xl rounded-xl shadow-lg shadow-black text-center flex flex-col items-center justify-end">
-          <p className="w-full h-1/2 text-6xl font-semibold">-10.1 %</p>
-          <p className="w-full h-1/4 text-xs mt-2">
-            PROMEDIO MÓVIL (AGO '23 - AGO-24')
+        <div className="w-1/4 h-32 bg-slate-700 text-gray-200 text-xl rounded-xl shadow-lg shadow-black text-center flex flex-col items-center justify-end">
+          <p className="w-full h-1/2 text-6xl font-semibold">-3.1 %</p>
+          <p className="w-full h-1/4 text-xs mt-2 tracking-wider">
+            <strong className="text-blue-400">EMPLEO</strong> (INTERANUAL)
           </p>
         </div>
       </div>
-      <div className="text-center flex items-center justify-center text-sm w-1/4 h-12 bg-red-200 rounded shadow shadow-red-800">
+      <div className="text-center flex items-center justify-center text-sm w-1/4 h-12 bg-slate-200 rounded shadow shadow-slate-800">
         <a
           className="w-full flex items-center justify-center font-semibold"
-          href="https://ciccra.com.ar/wp-content/uploads/2024/09/Inf-No-283b-2024-agosto.pdf"
+          href="https://www.adimra.org.ar/download/4201"
         >
           {" "}
           INFORME ORIGINAL <FaExternalLinkAlt className="ml-2" size={10} />
